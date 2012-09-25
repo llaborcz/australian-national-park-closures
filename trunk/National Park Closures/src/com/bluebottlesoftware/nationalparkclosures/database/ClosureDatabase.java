803a1007e87db30dd09cab56776952354cf35d7f
labor.lg <labor.lg@9a2c754f-ae85-2ea0-f2f6-db1a2bf4899e>
2012-09-25 21:15:37 +0000

    adding database classes

--- trunk/National Park Closures/src/com/bluebottlesoftware/nationalparkclosures/database/ClosureDatabase.java
+++ trunk/National Park Closures/src/com/bluebottlesoftware/nationalparkclosures/database/ClosureDatabase.java
@@ -1,41 +1,61 @@
 package com.bluebottlesoftware.nationalparkclosures.database;
 
-import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
-import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Database that stores information from the various closure RSS feeds
  */
 public class ClosureDatabase 
 {
-    SQLiteDatabase m_db;    /**<Backing database*/
+    private static final String CLOSURE_TABLE_NAME = "closuretable";
+    private static final String COLUMN_ID = "_id";  // This is a well known column name in SQLite
+    private static final String COLUMN_STATE = "state";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_DATE = "date";
+    private static final String COLUMN_LINK = "link";
+    private static final String COLUMN_GUID = "guid";
+    private static final String COLUMN_CATEGORY = "category";
+
+    // Emnumeration of states
+    public enum State
+    {
+        NSW,
+        QLD,
+        VIC,
+        TAS,
+        WA,
+        SA,
+        NT
+    }
+    
+    // Raw SQL to create the database table
+    private static final String CREATE_CLOSURE_TABLE = 
+        "CREATE TABLE "  + CLOSURE_TABLE_NAME + 
+        COLUMN_ID    + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+        COLUMN_STATE + " INTEGER," +
+        COLUMN_TITLE + " TEXT," + 
+        COLUMN_DATE  + " TEXT," + 
+        COLUMN_LINK  + " TEXT," +
+        COLUMN_GUID  + " TEXT," +
+        COLUMN_CATEGORY + " TEXT);";
+        
+    // Raw SQL to drop the database closure table
+    private static final String DROP_CLOSURE_TABLE = "DROP TABLE IF EXISTS "+CLOSURE_TABLE_NAME;
     
     /**
-     * Open helper for closure database 
-     * @author lee
-     *
+     * Creates the actual tables into the database
+     * @param db
      */
-    private class ClosureDatabaseHelper extends SQLiteOpenHelper
+    public static void createTables(SQLiteDatabase db)
     {
+        db.execSQL(CREATE_CLOSURE_TABLE);
+    }
 
-        public ClosureDatabaseHelper(Context context, String name,CursorFactory factory, int version)
-        {
-            super(context, name, factory, version);
-        }
-
-        @Override
-        public void onCreate(SQLiteDatabase db)
-        {
-            
-        }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
-        {
-            
-        }
-        
+    /**
+     * Drops the actual tables
+     */
+    public static void dropTables(SQLiteDatabase db)
+    {
+        db.execSQL(DROP_CLOSURE_TABLE);
     }
 }
