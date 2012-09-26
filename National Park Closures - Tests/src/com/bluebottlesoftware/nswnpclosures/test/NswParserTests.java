618310b36d0aa7e5fec4d48851a0bea3d72b06e2
labor.lg <labor.lg@9a2c754f-ae85-2ea0-f2f6-db1a2bf4899e>
2012-09-26 21:05:26 +0000

    Fixing build warnings

--- National Park Closures - Tests/src/com/bluebottlesoftware/nswnpclosures/test/NswParserTests.java
+++ National Park Closures - Tests/src/com/bluebottlesoftware/nswnpclosures/test/NswParserTests.java
@@ -1,9 +1,7 @@
 package com.bluebottlesoftware.nswnpclosures.test;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
