96580004662e5eb6b5be0dfb312ce42f5f6399ca
labor.lg <labor.lg@9a2c754f-ae85-2ea0-f2f6-db1a2bf4899e>
2012-09-26 21:01:35 +0000

    Adding initial test case for NSW feeds

---
+++ National Park Closures - Tests/src/com/bluebottlesoftware/nswnpclosures/test/NswParserTests.java
@@ -0,0 +1,74 @@
+package com.bluebottlesoftware.nswnpclosures.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.xml.sax.SAXException;
+
+import android.test.ActivityTestCase;
+import android.util.Log;
+
+import com.bluebottlesoftware.nationalparkclosures.parsers.FeedItem;
+import com.bluebottlesoftware.nationalparkclosures.parsers.NswFeedParser;
+
+/**
+ * Contains various test cases for the NSW data feed parser
+ */
+public class NswParserTests extends ActivityTestCase
+{
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+    }
+
+    protected void tearDown() throws Exception
+    {
+        super.tearDown();
+    }
+    
+    public void testBasicParse() throws XPathExpressionException, SAXException, IOException, ParserConfigurationException
+    {
+
+        InputStream stream = this.getInstrumentation().getContext().getResources().openRawResource(R.raw.nswfeed);
+        NswFeedParser parser = NswFeedParser.createFromStream(stream);
+        List<FeedItem> items = parser.getFeedItems();
+        for(FeedItem item : items)
+        {
+            String date = item.getDate();
+            assertNotNull(date);
+            
+            String category = item.getCategory();
+            assertNotNull(category);
+            
+            String description = item.getDescription();
+            assertNotNull(description);
+            
+            String guid = item.getGuid();
+            assertNotNull(guid);
+            
+            String link = item.getLink();
+            assertNotNull(link);
+            
+            String title = item.getTitle();
+            assertNotNull(title);
+            
+            Log.d("item",item.toString());
+        }
+    }
+    
+    public void testParseOfCorruptFile()
+    {
+        
+    }
+    
+    public void testParseOfEmptyData()
+    {
+        
+    }
+}
