9b1efc33ca352f588cf67d1e320d26c58e92a63f
labor.lg <labor.lg@9a2c754f-ae85-2ea0-f2f6-db1a2bf4899e>
2012-10-03 13:43:11 +0000

    adding more unit tests for reading from network

---
+++ trunk/National Park Closures - Tests/src/com/bluebottlesoftware/nationalparkclosures/TestData/TestUtils.java
@@ -0,0 +1,57 @@
+package com.bluebottlesoftware.nationalparkclosures.TestData;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+import android.database.Cursor;
+
+import com.bluebottlesoftware.nationalparkclosures.database.FeedDatabase;
+import com.bluebottlesoftware.nationalparkclosures.parsers.FeedItem;
+
+public class TestUtils
+{
+    public static void matchDatasets(Cursor c, List<FeedItem> items)
+    {
+        TestCase.assertEquals(c.getCount(), items.size());
+        List<FeedItem>cursorItems = FeedDatabase.getItemsForCursor(c);
+        for(FeedItem item : cursorItems)
+        {
+            assertItemOnList(item,items);
+        }
+    }
+
+    public static void assertItemOnList(FeedItem item, List<FeedItem> items)
+    {
+        boolean bFound = false;
+        for(FeedItem compare : items)
+        {
+            if(compare.sameAs(item))
+            {
+                bFound = true;
+                break;
+            }
+        }
+        TestCase.assertEquals(true, bFound);
+    }
+    
+    /**
+     * Asserts that the list of items is in decreasing order. Cursor must have at least two entries
+     * @param items
+     */
+    public static void assertListIsDecreasing(Cursor c)
+    {
+        assert(c.getCount() >= 2);
+        FeedItem previousItem = null;
+        List<FeedItem> items = FeedDatabase.getItemsForCursor(c);
+        for(FeedItem item : items)
+        {
+            if(previousItem != null)
+            {
+                long prevDateAsMs = item.getDateAsms();
+                long dateInMs = item.getDateAsms();
+                assert(prevDateAsMs >= dateInMs);
+            }
+            previousItem = item;
+        }
+    }
+}
