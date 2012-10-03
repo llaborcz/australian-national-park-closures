fbdc7c2f976ac8d3c592d1f7563cad6b457438d0
labor.lg <labor.lg@9a2c754f-ae85-2ea0-f2f6-db1a2bf4899e>
2012-10-03 19:36:04 +0000

    Fixing unit test data which was not being properly evaluated before

--- National Park Closures - Tests/src/com/bluebottlesoftware/nationalparkclosures/TestData/TestConstants.java
+++ National Park Closures - Tests/src/com/bluebottlesoftware/nationalparkclosures/TestData/TestConstants.java
@@ -5,5 +5,5 @@ public class TestConstants
     public static final int NumNswValidEntries = 79;    /// Count of valid entries in valid nsw data set
     public static final int NumNswValid78Entries = 78;    /// Count of valid entries in valid nsw 78 data set
     public static final String SingleEntryCategory = "hazard reduction burns";
-    public static final String TwoCategories = "hazard reduction burns;flooding";
+    public static final String TwoCategories = "hazard reduction burns,flooding";
 }
