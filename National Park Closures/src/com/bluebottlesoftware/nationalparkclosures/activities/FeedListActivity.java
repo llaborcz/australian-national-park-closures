6afb8d3c427f6c0b2bc5f42facc6fe8e3a7a84ce
labor.lg <labor.lg@9a2c754f-ae85-2ea0-f2f6-db1a2bf4899e>
2012-10-14 18:52:18 +0000

    Updating fragment code and activity tile. Removed fragment layout as it's not needed.

--- National Park Closures/src/com/bluebottlesoftware/nationalparkclosures/activities/FeedListActivity.java
+++ National Park Closures/src/com/bluebottlesoftware/nationalparkclosures/activities/FeedListActivity.java
@@ -1,5 +1,9 @@
 package com.bluebottlesoftware.nationalparkclosures.activities;
 
+import com.bluebottlesoftware.nationalparkclosures.data.FeedDataAdapter;
+import com.bluebottlesoftware.nationalparkclosures.data.Region;
+import com.bluebottlesoftware.nationalparkclosures.database.DatabaseHelper;
+import com.bluebottlesoftware.nationalparkclosures.database.FeedDatabase;
 import com.bluebottlesoftware.nationalparkclosures.fragments.FeedListFragment;
 import com.bluebottlesoftware.nswnpclosures.R;
 
@@ -7,7 +11,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.util.Log;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.Menu;
 
 /**
@@ -16,12 +21,33 @@ import android.view.Menu;
  * TODO Change the icon
  * TODO Change the title to match the state that's being displayed
  */
-public class FeedListActivity extends Activity implements RefreshListener
+public class FeedListActivity extends Activity
 {
+    private static final String CURRENTREGIONKEY = "currentregion"; // Key for the current region being viewed
+    
+    private int mRegion; // Region being viewed
+    
+    /**
+     * Saved state:
+     * State being viewed
+     */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        if(savedInstanceState != null)
+        {
+            // We've got a saved state
+            mRegion = savedInstanceState.getInt(CURRENTREGIONKEY);
+            setTitle(Region.getAsStringId(mRegion));
+        }
+        else
+        {
+            // Set the region to the default region
+            mRegion = Region.Nsw;
+            setTitle(R.string.nsw);
+        }
+        
         setContentView(R.layout.listviewactivity);
         FragmentManager fm = getFragmentManager();
         FeedListFragment listFragment = (FeedListFragment) fm.findFragmentById(R.id.listFragmentContent);  
@@ -33,21 +59,26 @@ public class FeedListActivity extends Activity implements RefreshListener
             ft.add(R.id.listFragmentContent, listFragment);
             ft.commit();  
         }
-        listFragment.setRefreshRequestedListener(this);
+        
+        SQLiteDatabase db = new DatabaseHelper(this).getReadableDatabase();
+        Cursor c = FeedDatabase.getItemsForStateSortedByDate(db, Region.Nsw);
+        listFragment.setListAdapter(new FeedDataAdapter(this, c, 0));
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-
+    
     /**
-     * Callback from listfragment
+     * Saves the current state: 
+     * List being viewed
      */
     @Override
-    public void onRefreshRequestedFromFragment()
+    public void onSaveInstanceState(Bundle outState)
     {
-        Log.d("FeedListActivity","Entry");
+        outState.putInt(CURRENTREGIONKEY,mRegion);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) 
+    {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
     }
 }
--- National Park Closures/src/com/bluebottlesoftware/nationalparkclosures/fragments/FeedListFragment.java
+++ National Park Closures/src/com/bluebottlesoftware/nationalparkclosures/fragments/FeedListFragment.java
@@ -1,14 +1,8 @@
 package com.bluebottlesoftware.nationalparkclosures.fragments;
 
-import com.bluebottlesoftware.nationalparkclosures.activities.RefreshListener;
-import com.bluebottlesoftware.nswnpclosures.R;
-
 import android.app.ListFragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ListView;
 
 /**
@@ -18,34 +12,10 @@ import android.widget.ListView;
  */
 public class FeedListFragment extends ListFragment
 {
-    private RefreshListener mListener;
-    
-    public void setRefreshRequestedListener(RefreshListener listener)
-    {
-        mListener = listener;
-    }
-    
     @Override
     public void onActivityCreated(Bundle savedInstanceState) 
     {
         super.onActivityCreated(savedInstanceState);
-    }
-    
-    @Override
-    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
-        View listView = inflater.inflate(R.layout.nationalparkeventlayout,container,false);
-        Button refreshButton = (Button) listView.findViewById(R.id.refreshButton);
-        
-        refreshButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                mListener.onRefreshRequestedFromFragment();
-            }
-        });
-        return listView;
     }
     
     /**
