b612ee4a020a1e67619b81b26f9bc418e3d0b44a
labor.lg <labor.lg@9a2c754f-ae85-2ea0-f2f6-db1a2bf4899e>
2012-10-21 16:30:00 +0000

    Moving logic to deal with the options menu into the fragment and out of the activity.

--- trunk/National Park Closures/src/com/bluebottlesoftware/nationalparkclosures/activities/FeedListActivity.java
+++ trunk/National Park Closures/src/com/bluebottlesoftware/nationalparkclosures/activities/FeedListActivity.java
@@ -8,9 +8,6 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 /**
  * This is the activity that displays the feed for a particular state. 
@@ -18,13 +15,11 @@ import android.view.MenuItem;
  * TODO Change the icon
  * TODO Change the title to match the state that's being displayed
  */
-public class FeedListActivity extends Activity implements FeedListCallbacks
+public class FeedListActivity extends Activity
 {
     private static final String CURRENTREGIONKEY = "currentregion"; // Key for the current region being viewed
     private int mRegion;                    // Region being viewed
     private FeedListFragment mListFragment; // Our retained list fragment
-    private MenuItem mRefresh;
-    private boolean mbRefresh;
     
     /**
      * Saved state:
@@ -46,7 +41,6 @@ public class FeedListActivity extends Activity implements FeedListCallbacks
             ft.commit();  
         }
         
-        mListFragment.setActivityCallbacks(this);
         if(savedInstanceState == null)
         {
             // Set the region to the default region
@@ -70,62 +64,5 @@ public class FeedListActivity extends Activity implements FeedListCallbacks
     {
         super.onSaveInstanceState(outState);
         outState.putInt(CURRENTREGIONKEY,mRegion);
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) 
-    {
-        super.onCreateOptionsMenu(menu);
-        
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected (MenuItem item)
-    {
-        boolean bResult = false;
-        switch(item.getItemId())
-        {
-        case R.id.menu_refresh:
-            mListFragment.refreshFeed();
-            bResult = true;
-            break;
-        }
-        return bResult;
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu)
-    {
-        super.onPrepareOptionsMenu(menu);
-        mRefresh = menu.findItem(R.id.menu_refresh);
-        if(mbRefresh)
-        {
-            mRefresh.setActionView(R.layout.refresh_menuitem);
-        }
-        return true; 
-    }
-    
-    @Override
-    public void onRefreshStarted()
-    {
-        mbRefresh = true;
-        if(mRefresh != null)
-        {
-            mRefresh.setActionView(R.layout.refresh_menuitem);
-        }
-    }
-
-    @Override
-    public void onRefreshFinished()
-    {
-        mbRefresh = false;
-        mRefresh.setActionView(null);
-    }
-
-    @Override
-    public void onListEntrySelected(long rowId)
-    {
     }
 }
