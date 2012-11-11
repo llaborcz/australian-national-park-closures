0d20585aac72f26a5bd60a116179538801c0fd1b
labor.lg <labor.lg@9a2c754f-ae85-2ea0-f2f6-db1a2bf4899e>
2012-11-11 21:16:46 +0000

    moving to compiled xpath expressions

--- National Park Closures/src/com/bluebottlesoftware/nationalparkclosures/parsers/FeedParser.java
+++ National Park Closures/src/com/bluebottlesoftware/nationalparkclosures/parsers/FeedParser.java
@@ -9,6 +9,7 @@ import java.util.List;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
@@ -38,7 +39,37 @@ public class FeedParser
     private static final String GeoLongQuery = "long";
     private static final String GeoRssPointQuery = "point";
     private static final String GeoRssCollectionPointQuery = "collection/point";
+    
+    private static final String LeadingSlash = "/";
+    
     private ArrayList<FeedItem> m_items = new ArrayList<FeedItem>();    // Stores our list of nodes
+    
+    private XPathExpression mItemQuery;
+    private XPathExpression mDateQuery;
+    private XPathExpression mTitleQuery;
+    private XPathExpression mLinkQuery;
+    private XPathExpression mGuidQuery;
+    private XPathExpression mCategoryQuery;
+    private XPathExpression mDescriptionQuery;
+    private XPathExpression mGeoLatQuery;
+    private XPathExpression mGeoLongQuery;
+    private XPathExpression mGeoRssPointQuery;
+    private XPathExpression mGeoRssCollectionPointQuery;
+    
+    private void compileXpathExpressions(XPath xpath) throws XPathExpressionException
+    {
+        mItemQuery = xpath.compile(ItemQuery);
+        mDateQuery = xpath.compile(DateQuery);
+        mTitleQuery = xpath.compile(TitleQuery);
+        mLinkQuery  = xpath.compile(LinkQuery);
+        mGuidQuery  = xpath.compile(GuidQuery);
+        mCategoryQuery = xpath.compile(CategoryQuery);
+        mDescriptionQuery = xpath.compile(DescriptionQuery);
+        mGeoLatQuery = xpath.compile(GeoLatQuery);
+        mGeoLongQuery= xpath.compile(GeoLongQuery);
+        mGeoRssPointQuery = xpath.compile(GeoRssPointQuery);
+        mGeoRssCollectionPointQuery = xpath.compile(GeoRssCollectionPointQuery);
+    }
     
     /**
      * Prevent instantiation
@@ -81,13 +112,14 @@ public class FeedParser
      */
     private void parse(Document xmlDocument, XPath xpath) throws XPathExpressionException
     {
-        NodeList items = (NodeList) xpath.evaluate(ItemQuery, xmlDocument,XPathConstants.NODESET);
+        compileXpathExpressions(xpath);
+        NodeList items = (NodeList) mItemQuery.evaluate(xmlDocument,XPathConstants.NODESET);
         for(int item = 0;item < items.getLength();item++)
         {
             m_items.add(createItemFromXPath(items.item(item),xpath));
         }
     }
-
+    
     /**
      * Creates a FeedItem from the given XML node
      * @param item
@@ -99,18 +131,23 @@ public class FeedParser
     {
         String geoLat = null;
         String geoLong= null;
-        String date  = (String) xpath.evaluate(DateQuery, node,XPathConstants.STRING);
-        String title = (String) xpath.evaluate(TitleQuery, node,XPathConstants.STRING);
-        String link  = (String) xpath.evaluate(LinkQuery, node,XPathConstants.STRING);
-        String guid  = (String) xpath.evaluate(GuidQuery, node,XPathConstants.STRING);
-        String description = (String) xpath.evaluate(DescriptionQuery, node,XPathConstants.STRING);
-        String point   = (String) xpath.evaluate(GeoRssPointQuery,node,XPathConstants.STRING);
+        String date  = (String) mDateQuery.evaluate(node,XPathConstants.STRING);
+        String title = (String) mTitleQuery.evaluate(node,XPathConstants.STRING);
+        String link  = (String) mLinkQuery.evaluate(node,XPathConstants.STRING);
+        String guid  = (String) mGuidQuery.evaluate(node,XPathConstants.STRING);
+        String description = (String) mDescriptionQuery.evaluate(node,XPathConstants.STRING);
+        String point   = (String) mGeoRssPointQuery.evaluate(node,XPathConstants.STRING);
         if(TextUtils.isEmpty(point))
         {
-            point = (String)xpath.evaluate(GeoRssCollectionPointQuery, node,XPathConstants.STRING);
+            point = (String)mGeoRssCollectionPointQuery.evaluate(node,XPathConstants.STRING);
         }
         
-        if(!TextUtils.isEmpty(point))
+        if(TextUtils.isEmpty(point))
+        {
+            geoLat  = (String) mGeoLatQuery.evaluate(node,XPathConstants.STRING);
+            geoLong = (String) mGeoLongQuery.evaluate(node,XPathConstants.STRING);
+        }
+        else
         {
             // We've got a geo rss point
             String [] latlong = point.split(" ");
@@ -120,17 +157,17 @@ public class FeedParser
                 geoLong= latlong[1];
             }
         }
-        else        
-        {
-            geoLat  = (String) xpath.evaluate(GeoLatQuery, node,XPathConstants.STRING);
-            geoLong = (String) xpath.evaluate(GeoLongQuery, node,XPathConstants.STRING);
-        }
-        NodeList categories = (NodeList) xpath.evaluate(CategoryQuery, node,XPathConstants.NODESET);
+        
+        NodeList categories = (NodeList) mCategoryQuery.evaluate(node,XPathConstants.NODESET);
         ArrayList<String> categoryArray = new ArrayList<String>();
         for(int i=0;i<categories.getLength();i++)
         {
             Node catNode = categories.item(i);
-            String cat = catNode.getTextContent();
+            String cat = catNode.getTextContent().trim();
+            if(cat.startsWith(LeadingSlash))
+            {
+                cat = cat.substring(1);
+            }
             categoryArray.add(cat);
         }
         
