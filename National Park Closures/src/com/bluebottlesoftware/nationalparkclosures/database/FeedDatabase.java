0302412f5e0ed79e80f92c93ca2828c55b8f62bd
labor.lg <labor.lg@9a2c754f-ae85-2ea0-f2f6-db1a2bf4899e>
2012-11-18 21:46:03 +0000

    REmoving build warning

--- National Park Closures/src/com/bluebottlesoftware/nationalparkclosures/database/FeedDatabase.java
+++ National Park Closures/src/com/bluebottlesoftware/nationalparkclosures/database/FeedDatabase.java
@@ -9,7 +9,6 @@ import com.bluebottlesoftware.nationalparkclosures.parsers.FeedItem;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.os.Debug;
 import android.text.TextUtils;
 
 /**
