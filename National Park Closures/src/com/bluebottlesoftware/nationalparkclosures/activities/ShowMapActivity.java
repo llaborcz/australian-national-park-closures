d43ec8b6aabce611e5df436f0f4e812cbe24d746
labor.lg <labor.lg@9a2c754f-ae85-2ea0-f2f6-db1a2bf4899e>
2012-12-03 22:23:46 +0000

    migrating project to new android maps API v2

--- National Park Closures/src/com/bluebottlesoftware/nationalparkclosures/activities/ShowMapActivity.java
+++ National Park Closures/src/com/bluebottlesoftware/nationalparkclosures/activities/ShowMapActivity.java
@@ -1,20 +1,33 @@
 package com.bluebottlesoftware.nationalparkclosures.activities;
 
-import com.bluebottlesoftware.nationalparkclosures.fragments.MapViewFragment;
+import com.bluebottlesoftware.nationalparkclosures.database.DatabaseHelper;
+import com.bluebottlesoftware.nationalparkclosures.database.FeedDatabase;
 import com.bluebottlesoftware.parkclosures.R;
-import com.google.android.maps.MapActivity;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-public class ShowMapActivity extends MapActivity
+public class ShowMapActivity extends Activity
 {
     public static final String EXTRA_IDS = "ids";       // array of row ids that we need to display
     public static final String EXTRA_TITLE = "title";   // String to use as title
+    private MapFragment mMapFragment;
+    private long [] mRowIds;
     
     @Override 
     public void onCreate(Bundle savedInstanceState)
@@ -28,18 +41,67 @@ public class ShowMapActivity extends MapActivity
         title = (title == null) ? getString(R.string.mapActivityTitle) : title;
         actionBar.setTitle(title);
         setContentView(R.layout.mapactivity);
-        
+        mRowIds = intent.getLongArrayExtra(EXTRA_IDS);       
         FragmentManager fm = getFragmentManager();
-        MapViewFragment mapFragment = (MapViewFragment) fm.findFragmentById(R.id.mapFragmentContent);  
-        if (mapFragment == null) 
+        mMapFragment = (MapFragment) fm.findFragmentById(R.id.mapFragmentContent);  
+        if (mMapFragment == null) 
         {
-            mapFragment = MapViewFragment.createInstance(intent.getLongArrayExtra(EXTRA_IDS));
+            GoogleMapOptions options = new GoogleMapOptions();
+            options.zoomControlsEnabled(false);
+            options.tiltGesturesEnabled(true);
+            mMapFragment = MapFragment.newInstance(options);
             FragmentTransaction ft = fm.beginTransaction();
-            ft.add(R.id.mapFragmentContent, mapFragment);
+            ft.add(R.id.mapFragmentContent, mMapFragment);
             ft.commit();  
         }
     }
 
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        drawMapMarkers(mMapFragment,mRowIds);        
+    }
+    
+    private void drawMapMarkers(MapFragment mapFragment, long [] rowIds)
+    {
+        Cursor c = FeedDatabase.getCursorForRowIds(DatabaseHelper.getDatabaseInstance(this), rowIds);
+        if(c.moveToFirst())
+        {
+            
+            GoogleMap map = mapFragment.getMap();
+            if(null != map)
+            {
+                int titleColumnIndex = c.getColumnIndex(FeedDatabase.COLUMN_TITLE);
+                int latColumnIndex   = c.getColumnIndex(FeedDatabase.COLUMN_LATITUDE);
+                int longColumnIndex  = c.getColumnIndex(FeedDatabase.COLUMN_LONGTITUDE);
+                LatLng latLng = null;
+                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                while(!c.isAfterLast())
+                {
+                    String title = c.getString(titleColumnIndex);
+                    String lat   = c.getString(latColumnIndex);
+                    String lng   = c.getString(longColumnIndex);
+                    latLng = new LatLng(Double.valueOf(lat),Double.valueOf(lng));
+                    builder.include(latLng);
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.title(title);
+                    markerOptions.position(latLng);                    
+                    map.addMarker(markerOptions);
+                    c.moveToNext();
+                }
+                
+                Point size = new Point();
+                getWindowManager().getDefaultDisplay().getSize(size);
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(builder.build(),size.x,size.y,100);
+                //CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 8);
+                map.moveCamera(cameraUpdate);
+                
+            }
+        }
+        c.close();
+    }
+    
     /**
      * Handles the action bar home button
      */
@@ -53,11 +115,5 @@ public class ShowMapActivity extends MapActivity
             bResult = true;
         }
         return bResult;
-    }
-    
-    @Override
-    protected boolean isRouteDisplayed()
-    {
-        return false;
     }
 }
